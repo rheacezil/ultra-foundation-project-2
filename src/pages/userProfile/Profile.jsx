01b188e0b5b1f5b3381a0dc403f592a0111f7343
john paul araquel <103520276+johnpaul1998@users.noreply.github.com>
2022-09-01 00:58:50 +0800

    30%

--- src/pages/userProfile/Profile.jsx
+++ src/pages/userProfile/Profile.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import { storage } from "./firebaseConfig";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import React from "react";
 
 function Profile() {
   // State to store uploaded file
-  const [file, setFile] = useState("");
-  const handleUpload = (e) => {
-    e.preventDefault();
-  };
+
   return (
     <div className="container-sm d-flex justify-content-center align-items-center vh-100">
-      <div className="input-group mb-3">
-        <input type="file" className="form-control" />
-        <button className="input-group-text" id="basic-addon2">
-          Upload image
-        </button>
-      </div>
+      <h1>Hello world</h1>
     </div>
   );
 }
